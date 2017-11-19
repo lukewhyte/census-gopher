6bbf3b7c6706751e58bbbcb11ca0fdf5d594f400
lukewhyte <lukeawhyte@gmail.com>
2017-11-19 00:02:35 -0600

    rewrote parseArguments as typescript, added unit tests and interfaces

--- src/parseArguments/parseArguments.ts
+++ src/parseArguments/parseArguments.ts
@@ -1,20 +1,56 @@
-/********************** 
+import * as _ from 'lodash';
 
-this file needs to do the following:
+// interfaces
+import { ParsedArguments } from '../interfaces';
 
- - split the arguments into key value pairs
- - handle state, county, tract, blockgroup, target, variable, years and filename arguments
- - throw errors if there is a problem parsing any single argument
- - return a hash for each of the arguments and a isSuccessful parameter
+// this hash maps each argument key to a function
+// that should be applied to the corresponding value
+const keyMap = {
+	state: val => commaSplit(val),
+	county: val => commaSplit(val),
+	tract: val => commaSplit(val),
+	blockgroup: val => commaSplit(val),
+	ids: val => commaSplit(val),
+	years: val => commaSplit(val),
+	target: val => splitTarget(val),
+	filename: val => val,
+};
 
-**********************/
+// returns and array split on the commas, often will have length: 1
+export const commaSplit = (val: string) => {
+	return val.split(',');
+};
 
-const testFunc = args => {
-	return {
-		inputVars: {
-			hello: 'world',
-		}
+// this function is used to parse the target geography
+export const splitTarget = (val: string) => {
+	if (val.indexOf(':') === -1) {
+		return { key: val, val: '*' }; // this signifies that the target is all of blank, eg: all the census tracts in the scope
+	} else {
+		const [ target, scopeStr ] = val.split(':');
+		const scope = commaSplit(scopeStr);
+		return { key: target, val: scope };
 	}
 };
 
-export default testFunc;
+// splitArg takes each input arg and splits it on '=' into { key: '', val: '' }
+export const splitArg = (arg: string) => {
+	if (arg.indexOf('=') === -1) {
+		console.error(`Argument ${arg} is improperly formatted`);
+		return { key: 'malformed', val: null };
+	}
+	const pair = arg.split('=');
+	return { key: pair[0], val: pair[1] };
+};
+
+// controller manages the parsing of each argument
+export const controller = (result: ParsedArguments, arg: string) => {
+	const { key, val } = splitArg(arg);
+	if (!keyMap[key]) {
+		result.isSuccessful = false;
+	} else {
+		result.payload[key] = keyMap[key](val);
+	}
+	return result;
+};
+
+export default (args: Array<string>) => _.reduce(args, controller, { isSuccessful: true, payload: {} });
