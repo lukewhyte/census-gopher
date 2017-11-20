5af77ce22d08b9f78b84d01a9764293ef92264cd
lukewhyte <lukeawhyte@gmail.com>
2017-11-19 20:59:31 -0600

    setting up structure for next phase of building

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
 #! /usr/bin/env node
 
-import parseArguments from './parseArguments/parseArguments';
+import parseArguments 		from './parseArguments';
+import varMappingUtils		from './varMappingUtils';
+import getMissingGeography 	from './getMissingGeography';
 
 const vars = parseArguments(process.argv.slice(2));
+
+const { knownGeoKeys, unknownGeoKeys } = varMappingUtils.unpackGeoKeys(vars);
+
+const buildWorkbookHash = async vars => {
+	const mainQueryArr = await getMissingGeography(vars);
+};
 
 console.log(vars);
